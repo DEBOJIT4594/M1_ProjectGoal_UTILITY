055513272ba8168f27478c7dad3be1196f2e8685
DEBOJIT4594 <Debojit4594@gmail.com>
2022-02-10 21:49:15 +0530

    M1_CALCULATOR

---
+++ 3_Implementation/calculator.h
@@ -0,0 +1,17 @@
+#ifndef CALCULATOR_H_
+#define CALCULATOR_H_
+
+
+#include <stdio.h>  
+#include <math.h>  
+#include <stdlib.h> 
+
+    int option;
+    float N1;
+    float N2; 
+    int n1;
+    int n2; 
+    float RESULT;  
+    char ch;  
+
+#endif
