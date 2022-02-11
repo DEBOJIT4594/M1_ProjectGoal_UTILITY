b1df807da5b18c7627b92e5db3d390de9c7cdfa9
DEBOJIT4594 <Debojit4594@gmail.com>
2022-02-11 22:36:25 +0530

    M1_CALCULATOR

---
+++ 3_Implementation/Version_2/scr/calculator.h
@@ -0,0 +1,55 @@
+#include <stdio.h>  
+#include <math.h>  
+#include <stdlib.h> 
+
+    int option;
+    float N1;
+    float N2; 
+    int n1;
+    int n2; 
+    float RESULT;  
+    char ch;  
+
+    switch (option)  
+    {  
+        case 1:             
+        //WILL DO ADDITION
+
+        case 2:   
+        //WILL DO SUBSTRACTION              
+                
+        case 3:    
+        //WILL DO MULTIPLICATION          
+              
+        case 4: 
+        //WILL DO DIVISION              
+             
+        case 5:
+        //WILL PERFORM SIN FUNCTION
+           
+        case 6:
+        //WILL PERFORM COS FUNCTION
+            
+        case 7:    
+        //WILL PERFORM SQUARE ROOT             
+           
+        case 8:
+        //WILL FIND POWER OF NUMBER
+          
+        case 9:
+        //WILL PERFORM FACTORIAL OF NUMBER
+          
+    
+        case 10:
+        //WILL FIND PERCENTAGE
+           
+        case 11:  
+        //WILL PERFORM SQUARE OF NUMBER                         
+                                             
+        case 12:  
+        //WILL ASK TO CHOOSE ANY OPTION
+               
+        default:  
+                   
+    }
+#endif
